676c755d9fb6e7d9a666779bb21f80fd6a1336c2
ives-algorhym <algoives@icloud.com>
2026-03-05 15:24:12 -0500

    Crete UIKit project

---
+++ Apps/MySkillo/MySkillo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MySkillo
+//
+//  Created by Ives Murillo on 3/5/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
