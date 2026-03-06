80930c99b38aae3fa9ad9af587c037f52d8ac3ad
ives-algorhym <algoives@icloud.com>
2026-03-06 13:49:55 -0500

    restructure

---
+++ Packages/AppCore/Sources/AppCore/Environment.swift
@@ -0,0 +1,14 @@
+//
+//  Enviroment.swift
+//  
+//
+//  Created by Ives Murillo on 3/5/26.
+//
+
+
+public struct Environment : Sendable {
+    public let name: String
+
+    public static let production = Environment(name: "production")
+    public static let development = Environment(name: "development")
+}
