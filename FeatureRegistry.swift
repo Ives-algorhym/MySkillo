0a64ae22eba9d2b4ce9d81a85c7145721571dcf5
ives-algorhym <algoives@icloud.com>
2026-03-05 23:29:51 -0500

    Implemented Continer

---
+++ FeatureRegistry.swift
@@ -0,0 +1,22 @@
+//
+//  FeatureRegistry.swift
+//  
+//
+//  Created by Ives Murillo on 3/5/26.
+//
+
+
+public struct FeatureRegistry {
+
+    private let registrants: [DependnencyRegistering.Type]
+
+    public init(registrants: [DependnencyRegistering.Type]) {
+        self.registrants = registrants
+    }
+
+    public func registerAll(in continer: Continer) {
+        registrants.forEach {
+            $0.register(in: continer)
+        }
+    }
+}
