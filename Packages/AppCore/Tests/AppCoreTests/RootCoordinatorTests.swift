6346ae670eff9ea020b8c1647601d9ff56de63dd
ives-algorhym <algoives@icloud.com>
2026-03-11 17:06:20 -0500

    Add tests to root coordinator

--- Packages/AppCore/Tests/AppCoreTests/RootCoordinatorTests.swift
+++ Packages/AppCore/Tests/AppCoreTests/RootCoordinatorTests.swift
@@ -5,7 +5,36 @@
 //  Created by Ives Murillo on 3/9/26.
 //
 
+import FeatureContracts
 import Testing
+import UIKit
+@testable import AppCore
 
 @Suite("Root Coordinator Tests")
-struct RootCoordinatorTests {}
+struct RootCoordinatorTests {
+    @MainActor
+    @Test("start sets resume as a root view and just one tab")
+    func startSetsResumeAsRoot() {
+        // Arrange
+        let stub = StubResumeProvider()
+        let sut = RootCoordinator(resumeProvider: stub)
+
+        // Act
+        sut.start()
+        let root = sut.rootViewController
+
+        // Assert
+        #expect(root.viewControllers?.first == stub.lastBuildViewController)
+        #expect(root.tabBar.isHidden == true)
+    }
+}
+
+final class StubResumeProvider: ResumeFeatureProviding {
+    var lastBuildViewController: UIViewController?
+
+    func makeResumeViewController() -> UIViewController {
+        let controller = UIViewController()
+        lastBuildViewController = controller
+        return controller
+    }
+}
