72be1304f70711b1ed0617b41d62d2b461e4bbac
ives-algorhym <algoives@icloud.com>
2026-03-10 20:20:39 -0500

    refactor

--- Packages/Features/Resume/Tests/ResumeTests/ResumeTests.swift
+++ Packages/Features/Resume/Tests/ResumeTests/ResumeTests.swift
@@ -1,13 +1,5 @@
 //
-<<<<<<< HEAD
-<<<<<<< HEAD
-//  File.swift
-=======
 //  ResumeTests.swift
->>>>>>> 8a8b1bd (Update swift lint exclude rules)
-=======
-//  ResumeTests.swift
->>>>>>> 7f54c43 (Update swift lint exclude rule)
 //  Resume
 //
 //  Created by Ives Murillo on 3/10/26.
