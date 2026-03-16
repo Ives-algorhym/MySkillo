c08ad0057acdd645609076e24d6e0f5e34dfbde5
Ives <92604040+Ives-algorhym@users.noreply.github.com>
2026-03-16 15:00:50 -0500

    Create Logging package (#32)
    * Create Logging package
    
    * Create Logging package
    
    * Create Logging package

---
+++ Packages/Platform/Logging/Tests/LoggingTests/LoggingTests.swift
@@ -0,0 +1,14 @@
+//
+//  LoggingTests.swift
+//  Logging
+//
+//  Created by Ives Murillo on 3/10/26.
+//
+
+@testable import Logging
+import Testing
+
+@Suite()
+struct LoggingTests {
+    @Test func logging() {}
+}
