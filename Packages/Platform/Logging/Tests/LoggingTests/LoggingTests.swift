9073df9cb90ee7829f66e3f64c6c4198e42343cd
ives-algorhym <algoives@icloud.com>
2026-03-16 13:03:44 -0500

    Create Logging package

---
+++ Packages/Platform/Logging/Tests/LoggingTests/LoggingTests.swift
@@ -0,0 +1,13 @@
+//
+//  LoggingTests.swift
+//  Logging
+//
+//  Created by Ives Murillo on 3/10/26.
+//
+
+@testable import Logging
+import Testing
+
+@Test func example() async throws {
+    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+}
