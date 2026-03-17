8e6bdeeb931496ad8d7420eff4ee2521f202cf5e
ives-algorhym <algoives@icloud.com>
2026-03-16 22:21:07 -0500

    Add Logging package dependency to AppCore package

--- Packages/AppCore/Package.swift
+++ Packages/AppCore/Package.swift
@@ -13,12 +13,14 @@ let package = Package(
     ],
     dependencies: [
         .package(path: "../../Features/Resume"),
+        .package(path: "../../Platform/Logging")
     ],
     targets: [
         .target(
             name: "AppCore",
             dependencies: [
                 .product(name: "Resume", package: "Resume"),
+                .product(name: "Logging", package: "Logging")
             ]
         ),
         .testTarget(
