e6069d191b88e1f1d05105d9a6c5758ba313e843
ives-algorhym <algoives@icloud.com>
2026-06-01 22:39:24 -0700

    Create Domain Folder structure, add Entities Folder, add Take Files

---
+++ Packages/Features/Resume/Sources/Resume/Domain/Entities/Take.swift
@@ -0,0 +1,8 @@
+//
+//  Take.swift
+//  Resume
+//
+//  Created by Ives Murillo on 6/1/26.
+//
+
+import Foundation
---
+++ Packages/Features/Resume/Tests/ResumeTests/Domain/Entities/TakeTests.swift
@@ -0,0 +1,8 @@
+//
+//  TakeTests.swift
+//  Resume
+//
+//  Created by Ives Murillo on 6/1/26.
+//
+
+import Foundation
