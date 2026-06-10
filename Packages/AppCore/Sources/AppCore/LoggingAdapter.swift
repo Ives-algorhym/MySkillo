232b85e97a8c649b8c765e6c7b302ebf56acd09e
ives-algorhym <algoives@icloud.com>
2026-06-10 16:47:50 -0700

    test: scaffold LoggingAdapter and LoggingAdapterTests files

---
+++ Packages/AppCore/Sources/AppCore/LoggingAdapter.swift
@@ -0,0 +1,8 @@
+//
+//  LoggingAdapter.swift
+//  AppCore
+//
+//  Created by Ives Murillo on 6/10/26.
+//
+
+import Foundation
---
+++ Packages/AppCore/Tests/AppCoreTests/LoggingAdapterTests.swift
@@ -0,0 +1,8 @@
+//
+//  LoggingAdapterTests.swift
+//  AppCore
+//
+//  Created by Ives Murillo on 6/10/26.
+//
+
+import Foundation
