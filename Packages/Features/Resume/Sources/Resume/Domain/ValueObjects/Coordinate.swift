e5cb175bb4a6cebb3e199cb2e579fc0807918d18
ives-algorhym <algoives@icloud.com>
2026-06-13 19:00:27 -0700

    feat: Coordinate value object complete — all tests passing

---
+++ Packages/Features/Resume/Sources/Resume/Domain/ValueObjects/Coordinate.swift
@@ -0,0 +1,21 @@
+//
+//  Coordinate.swift
+//  Resume
+//
+//  Created by Ives Murillo on 6/12/26.
+//
+
+import Foundation
+
+struct Coordinate {
+    let latitude: Double
+    let longitude: Double
+
+    init?(latitude: Double, longitude: Double) {
+        guard latitude <= 90, latitude >= -90, longitude <= 180, longitude >= -180 else {
+            return nil
+        }
+        self.latitude = latitude
+        self.longitude = longitude
+    }
+}
--- Packages/Features/Resume/Tests/ResumeTests/ValueObjects/CoordinateTests.swift
+++ Packages/Features/Resume/Tests/ResumeTests/ValueObjects/CoordinateTests.swift
@@ -1,8 +1,51 @@
 //
-//  File.swift
+//  CoordinateTests.swift
 //  Resume
 //
 //  Created by Ives Murillo on 6/12/26.
 //
 
-import Foundation
+import Testing
+@testable import Resume
+
+@Suite("Coordinate Tests")
+struct CoordinateTests {
+
+    @Suite("Latitude Init")
+    struct LatitudeInitTests {
+
+        @Test
+        func whenLatitudeAbove90_returnsNil() {
+            #expect(Coordinate(latitude: 91, longitude: 0) == nil)
+        }
+
+        @Test
+        func whenLatitudeBelowNegative90_returnsNil() {
+            #expect(Coordinate(latitude: -91, longitude: 0) == nil)
+        }
+
+        @Test
+        func whenLatitudeIsValid_returnsInstance() {
+            #expect(Coordinate(latitude: 45, longitude: 0) != nil)
+        }
+    }
+
+    @Suite("Longitude Init")
+    struct LongitudeInitTests {
+
+        @Test
+        func whenLongitudeAbove180_returnsNil() {
+            #expect(Coordinate(latitude: 45, longitude: 181) == nil)
+        }
+
+        @Test
+        func whenLongitudeeBelowNegative180_returnsNil() {
+            #expect(Coordinate(latitude: 0, longitude: -181) == nil)
+        }
+
+        @Test
+        func whenLongitudeIsValid_returnsInstance() {
+            #expect(Coordinate(latitude: 0, longitude: 160) != nil)
+        }
+    }
+}
