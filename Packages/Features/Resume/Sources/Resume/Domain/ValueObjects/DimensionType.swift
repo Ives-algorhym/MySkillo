c3be6d23a509683d9d5d09f35d9d454c2658251a
ives-algorhym <algoives@icloud.com>
2026-06-30 18:24:52 -0700

    feat: DimensionType enum complete

---
+++ Packages/Features/Resume/Sources/Resume/Domain/ValueObjects/DimensionType.swift
@@ -0,0 +1,14 @@
+//
+//  DimensionType.swift
+//  Resume
+//
+//  Created by Ives Murillo on 6/30/26.
+//
+
+enum DimensionType: CaseIterable, Equatable {
+    case technicalDepth
+    case eyeContact
+    case pacing
+    case structure
+    case fillerWords
+}
