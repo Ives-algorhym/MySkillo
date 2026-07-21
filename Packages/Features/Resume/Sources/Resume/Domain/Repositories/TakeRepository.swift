0de99290bcefcde2a3440e0b747c8721c9117a17
ives-algorhym <algoives@icloud.com>
2026-07-21 11:44:50 -0700

    feat: TakeRepository protocol + MockTakeRepository

--- Packages/Features/Resume/Sources/Resume/Domain/Repositories/TakeRepository.swift
+++ Packages/Features/Resume/Sources/Resume/Domain/Repositories/TakeRepository.swift
@@ -10,4 +10,8 @@ import Foundation
 protocol TakeRepository {
     func save(_ take: Take) async throws
     func delete(id: UUID) async throws
+    func fetch(id: UUID) async throws -> Take
+    func fetchAll() async throws -> [Take]
+    func setActiveVideoResume(_ takeId: UUID) async throws
+    func fetchActiveVideoResume() async throws -> Take?
 }
--- Packages/Features/Resume/Tests/ResumeTests/Domain/Repositories/MockTakeRepository.swift
+++ Packages/Features/Resume/Tests/ResumeTests/Domain/Repositories/MockTakeRepository.swift
@@ -11,6 +11,7 @@ import Foundation
 actor MockTakeRepository: TakeRepository {
     private(set) var savedTakes: [Take] = []
     private(set) var deletedIds: [UUID] = []
+    private(set) var activeVideoResumeId: UUID?
 
     func save(_ take: Resume.Take) async throws {
         savedTakes.append(take)
@@ -18,5 +19,26 @@ actor MockTakeRepository: TakeRepository {
 
     func delete(id: UUID) async throws {
         deletedIds.append(id)
+    }
+
+    func fetch(id: UUID) async throws -> Take {
+        guard let take = savedTakes.first(where: { $0.id == id }) else {
+            throw TakeError.unknown
+        }
+
+        return take
+    }
+
+    func fetchAll() async throws -> [Take] {
+        savedTakes
+    }
+
+    func setActiveVideoResume(_ takeId: UUID) async throws {
+        activeVideoResumeId = takeId
+    }
+
+    func fetchActiveVideoResume() async throws -> Take? {
+        guard let id = activeVideoResumeId else { return nil }
+        return savedTakes.first(where: { $0.id == id })
     }
 }
